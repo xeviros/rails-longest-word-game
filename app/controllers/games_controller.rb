9986256ece4ef08e8be51c55a608138f7ef5f6fc
Xavier Ros <x.ros.martinez@gmail.com>
2018-06-04 17:43:36 +0200

    first part done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,37 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do |x|
+      x = ("A".."Z").to_a.sample
+      @letters << x
+    end
+    @letters
+  end
+
+  def score
+    @word = params[:query]
+    @letters = new.sort
+    @longest_word = longest_word(@word)
+  end
+
+  def longest_word(word)
+    @array = word.upcase.split('').sort
+    @letters = score
+    if @array.exclude? (@letters)
+      return "Sorry but #{@word} can't be built of #{@letters}"
+    else
+      return "good"
+    end
+
+
+
+    # if @word.include? "@letters"
+    #   return "Sorry but #{@word} can't be built of #{@letters}"
+    # else
+    #   return "good"
+    # end
+
+
+  end
+
+end
